b75e62114edf2ff05e2d5adc5a48da0f6c93f2e0
xp2000926 <3053949745@qq.com>
2024-08-04 22:56:33 +0800

    css方案选择

--- src/components/Button/stories/Custom.stories.tsx
+++ src/components/Button/stories/Custom.stories.tsx
@@ -1,6 +1,18 @@
-export const Custom = {
-  args: {
-    size: "small",
-    label: "Custom",
+import { Button } from "min-fish-ui";
+import { makeStyles } from "@griffel/react";
+
+const useClasses = makeStyles({
+  root: {
+    backgroundColor: "red",
+    padding: "100px",
   },
+});
+
+export const Custom = () => {
+  const styles = useClasses();
+  return (
+    <div className={styles.root}>
+      <Button primary label="Button" />
+    </div>
+  );
 };
